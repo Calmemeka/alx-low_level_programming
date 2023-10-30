dda536be564f8de97446434d8e288e40634d9f76
Calmemeka <favournebo4@gmail.com>
2023-10-30 02:07:54 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *main - write a program that prints putchar
+ *_putchar - puts the alphabet "putchar" to the output
+ *Return: always 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
